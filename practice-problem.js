c2274156321db337d08acd67e14946df87961123
Lexius Waltar <lexius.waltar@gmail.com>
2025-02-20 19:08:21 -0500

    Create practice-problem.js

---
+++ practice-problem.js
@@ -0,0 +1,12 @@
+/*
+Challenge Problem:
+
+You are designing a simple inventory system for a small store. Create an object called 'store' with the following properties:
+
+- name (store name)
+- location (store location)
+- inventory (an array of objects, each representing a product with properties: name, price, and quantity)
+- a method 'totalInventoryValue' that calculates and returns the total value of all products (price * quantity)
+
+Once you've created the store object, log the total inventory value by calling the method.
+*/
